3a81c1c80076083ce16159da14e370baf40bed5f
Dominik Klotz <dominik.klotz@askui.com>
2024-11-05 09:46:46 +0100

    feat: add new AskUI Controller to docker image

--- askui-ui-controller/chrome/start-askui-ui-controller.sh
+++ askui-ui-controller/chrome/start-askui-ui-controller.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 VERBOSE=${VERBOSE:-""}
 ASKUI_CONTROLLER_ARGS=${ASKUI_CONTROLLER_ARGS:-""}
@@ -6,4 +7,8 @@ if [ -n "$VERBOSE" ]; then
     ASKUI_CONTROLLER_ARGS="$ASKUI_CONTROLLER_ARGS --log-level=verbose"
 fi
 
-/askui-ui-controller.AppImage --appimage-extract-and-run  --no-sandbox -m -d 0 --host "0.0.0.0" ${ASKUI_CONTROLLER_ARGS}
+ls -la /home/seluser/.askui-suites/Binaries/resources/assets/binaries/AskuiRemoteDeviceController
+
+/home/seluser/.askui-suites/Binaries/resources/assets/binaries/AskuiRemoteDeviceController --showOverlay False &
+
+/home/seluser/.askui-suites/Binaries/askui-controller --no-sandbox -m -d 0 --host "0.0.0.0" ${ASKUI_CONTROLLER_ARGS}
