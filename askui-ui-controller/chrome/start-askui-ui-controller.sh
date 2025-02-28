3d5391e48775b65f99de23c64dc4eaac4dc1bca9
Dominik Klotz <dominik.klotz@askui.com>
2025-02-28 13:20:50 +0100

    Update to AskUI Suite 24.12.1

--- askui-ui-controller/chrome/start-askui-ui-controller.sh
+++ askui-ui-controller/chrome/start-askui-ui-controller.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 VERBOSE=${VERBOSE:-""}
 ASKUI_CONTROLLER_ARGS=${ASKUI_CONTROLLER_ARGS:-""}
@@ -6,9 +7,10 @@ if [ -n "$VERBOSE" ]; then
     ASKUI_CONTROLLER_ARGS="$ASKUI_CONTROLLER_ARGS --log-level=verbose"
 fi
 
+
 while ! xdpyinfo -display $DISPLAY > /dev/null 2>&1; do
   echo "Waiting for xvfb on display ${DISPLAY} ... "
   sleep 1
 done
 
-/home/seluser/.askui-suites/Binaries/askui-controller --no-sandbox -m -d 0 --host "0.0.0.0" ${ASKUI_CONTROLLER_ARGS}
+/home/seluser/.askui-suites/Binaries/askui-controller --no-sandbox -m -d 0 --host "0.0.0.0" --hide-overlay ${ASKUI_CONTROLLER_ARGS}
